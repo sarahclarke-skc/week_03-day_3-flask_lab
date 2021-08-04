6e36621d20c4ae0a5bb38a19706f2637007e573d
sarahclarke-skc <sarahkathrynclarke@gmail.com>
2021-08-04 13:52:17 +0100

    order_list.py set up

--- models/order.py
+++ models/order.py
@@ -1,6 +1,8 @@
 class Order:
-    def __init__(self, customer_name, order_date, quantity, total_cost):
+    def __init__(self, customer_name, order_date, book, total_cost):
         self.customer_name = customer_name
         self.order_date = order_date
-        self.quantity = quantity
+        self.book = book
         self.total_cost = total_cost
+
+        
--- models/order_list.py
+++ models/order_list.py
@@ -1,2 +1,11 @@
 from models.order import *
 from models.book import *
+
+book1 = Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 750)
+book2 = Book("Harry Potter and the Chamber of Secrets", "J.K. Rowling", 750)
+book3 = Book("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", 750)
+
+order1 = Order("John O'Neil", "2 August 2021", book1, book1.price)
+order2 = Order("Sean O'Grady", "3 August 2020", book1, book1.price)
+order3 = Order("Carrie Potter", "4 August 2019", book1, book1.price)
+orders =[order1, order2, order3]
